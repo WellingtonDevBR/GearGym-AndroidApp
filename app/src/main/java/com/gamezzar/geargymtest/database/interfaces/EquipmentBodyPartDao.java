62433fb82436ce512e3489ef30a326506437b270
Wellington <wellingtonaustralia@gmail.com>
2024-03-21 02:56:17 +1100

    reorganizing entities and juntion table for the application. Bug fixes.

---
+++ app/src/main/java/com/gamezzar/geargymtest/database/interfaces/EquipmentBodyPartDao.java
@@ -0,0 +1,10 @@
+package com.gamezzar.geargymtest.database.interfaces;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+
+@Dao
+public interface EquipmentBodyPartDao {
+    @Insert
+    void insert(Equi)
+}
--- app/src/main/java/com/gamezzar/geargymtest/database/interfaces/RoutineWorkoutDao.java
+++ app/src/main/java/com/gamezzar/geargymtest/database/interfaces/RoutineWorkoutDao.java
@@ -3,12 +3,12 @@ package com.gamezzar.geargymtest.database.interfaces;
 import androidx.room.Dao;
 import androidx.room.Insert;
 
-import com.gamezzar.geargymtest.database.entities.WorkoutRoutine;
+import com.gamezzar.geargymtest.database.entities.RoutineWorkout;
 
 @Dao
 public interface WorkoutRoutineDao {
     @Insert
-    void insert(WorkoutRoutine workoutRoutine);
+    void insert(RoutineWorkout routineWorkout);
 
     // CRUD operations for WorkoutRoutine
     // Method to retrieve a specific WorkoutRoutine with its sets
